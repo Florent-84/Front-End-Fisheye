34d25f47b50d4b4553fa39c9dd58d0509c0d1b4a
Florent-84 <florent.tuppo@live.fr>
2022-04-17 15:29:29 +0200

    refactorisation du formulaire

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,62 +1,76 @@
 
-/*********************************** ouverture du formulaire *******************************************/
+/******************************************* ouverture du formulaire (avec sa fermeture image croix)***************************************************/
 
-function displayModal(photographerName) {
+function displayModal(photographer) {
 
-    const modal              = document.getElementById("contact_modal");
-    const headerLogo         = document.getElementsByTagName("header");                                                
-    const headerPhotographer = document.getElementsByClassName("photograph-header");
-    const h2                 = document.getElementsByTagName("h2");                             
-    const filter             = document.getElementsByTagName("label");                                          
+    const modal              = document.getElementById("contact_modal");               // récupération des éléments
+    const headerLogo         = document.getElementsByTagName("header")[0];                                                
+    const headerPhotographer = document.getElementsByClassName("photograph-header")[0];
+    const nameForm           = document.getElementsByClassName("name-for-form")[0];                            
+    const filter             = document.getElementsByTagName("label")[0];                                          
     const mediaSection       = document.getElementById("media_section");                               
     const firstNameInput     = document.getElementById("prenom");
     const lastNameInput      = document.getElementById("nom");
     const emailInput         = document.getElementById("email");
     const messageInput       = document.getElementById("message");
-    const errorFirst         = document.getElementById("error-first");                              // les balises d'erreur
+    const errorFirst         = document.getElementById("error-first");                 // les balises d'erreur
     const errorLast          = document.getElementById("error-last");
     const errorMail          = document.getElementById("error-email");
     const errorMessage       = document.getElementById("error-message");
-    const btnSubmit          = document.getElementsByClassName("contact_button");    
-    const regexFirstLast     = /^[A-Za-z]{2,30}$/;                                                  // les regex  
+    const btnSubmit          = document.getElementsByClassName("contact_button")[1];
+    const imgCloseModal      = document.getElementById('close-form-img');              // croix de la fermeture modal    
+    const regexFirstLast     = /^[A-Za-z]{2,30}$/;                                     // les regex  
     const regexEmail         = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/; 
-    const regexMessage       = /^[a-zA-Z0-9-_.]{2,30}$/;                                            // regex message
+    const regexMessage       = /^[a-zA-Z0-9-_.]{2,30}$/;                                  
 
-    modal.style.display                 = "block";                                                  // on affiche le formulaire
-    headerLogo[0].style.display         = "none";                                                   // on cache le header avec logo
-    headerPhotographer[0].style.display = "none";                                                   // on cache le header du photographe 
-    filter[0].style.display             = "none";                                                   // on cache le filtre 
-    mediaSection.style.display          = "none";                                                   // on cache la section media
-    h2[0].innerHTML                     = `Contactez-moi<br>${photographerName}`;
-    h2[0].setAttribute                    ('tabindex', '0');
+    modal.style.display                 = "block";                           // affiche la modale
+    headerLogo.style.display            = "none";                            // cache le header avec logo
+    headerPhotographer.style.display    = "none";                            // cache le header du photographe 
+    filter.style.display                = "none";                            // cache le filtre 
+    mediaSection.style.display          = "none";                            // cache la section media
+    nameForm.className                  = "name-for-form";
+    nameForm.innerHTML                  = "Contactez moi<br>"+ photographer;
+    nameForm.setAttribute                 ('tabindex', '0');
 
-    /************************** fonction pour vider toute les erreurs et tous les champs *****************************/
-    function resetForm() {                                                          
-        firstNameInput.value                = " ";                                                                                            
-        lastNameInput.value                 = " ";
-        emailInput.value                    = " ";
-        messageInput.value                  = " ";
-        errorFirst.style.display            = "none";
-        errorLast.style.display             = "none";
-        errorMail.style.display             = "none";
-        errorMessage.style.display          = "none";
-    }
-    /************************** fonction pour réafficher les éléments et enlever la modale *****************************/
+    /*** fonction pour réafficher tous les éléments du photographe et enlever la modale ***/
     function redisplayElements() {                                                  
-        modal.style.display                 = "none";                                   
-        headerLogo[0].style.display         = "block";                                  
-        headerLogo[0].style.display         = "flex";
-        headerPhotographer[0].style.display = "block";                                  
-        headerPhotographer[0].style.display = "flex"; 
-        filter[0].style.display             = "block";                                  
-        mediaSection.style.display          = "block";                                  
-        mediaSection.style.display          = "flex";                                                                 
+        modal.style.display              = 'none'; // cache la modale 
+        headerLogo.style.display         = 'flex'; // affiche header avec logo                                 
+        headerPhotographer.style.display = 'flex'; // affiche le header du photographe                            
+        filter.style.display             = "flex"; // affiche le filtre                           
+        mediaSection.style.display       = "flex"; // affiche la section media                                                                 
     }
 
-    /********************************************** vérification des inputs du formulaire au click submit ****************************************/
+    /*** fonction pour enlever les balises erreurs et vider les champs du formulaire ***/
+    function resetForm() { 
+        firstNameInput.value       = " ";
+        lastNameInput.value        = " ";
+        emailInput.value           = " ";
+        messageInput.value         = " ";
+        errorFirst.style.display   = "none";      
+        errorLast.style.display    = "none";
+        errorMail.style.display    = "none";
+        errorMessage.style.display = "none";                                                        
+    }
 
-    /********** fonction controle du prénom et du nom ********/                                                                     
-    btnSubmit[1].addEventListener('click', validateFirstLast);                 
+    /**** fonction fermeture du formulaire sur image croix au click ****/
+    imgCloseModal.addEventListener('click', closeModal);
+    function closeModal() {
+        redisplayElements();
+        resetForm();                                                                                       
+    }
+    
+    /**** fonction fermeture du formulaire sur image croix avec touche entrée ****/
+    imgCloseModal.addEventListener('keydown', (e) => {
+        if (e.key == 'Enter') {
+            closeModal();
+        }
+    });
+
+    /*********************************** vérification des inputs du formulaire 1 par 1 au click submit *************************************/
+
+    /***** controle du prénom et du nom (même regex) *****/                                                                     
+    btnSubmit.addEventListener('click', validateFirstLast);                 
         function validateFirstLast(e) {                                            
             if (!regexFirstLast.test(firstNameInput.value && lastNameInput.value)) {
                 e.preventDefault();
@@ -64,90 +78,45 @@ function displayModal(photographerName) {
                 errorLast.style.display   = "block";
             } if (regexFirstLast.test(firstNameInput.value)) {
                 errorFirst.style.display  = "none";
-                console.log(firstNameInput.value);
+                console.log("prénom : " + firstNameInput.value);
             } if (regexFirstLast.test(lastNameInput.value)) {
                 errorLast.style.display   = "none";
-                console.log(lastNameInput.value);
-                
+                console.log("nom : " + lastNameInput.value);    
             }      
         }
 
-    /********** fonction controle de l'email ************/     
-    btnSubmit[1].addEventListener('click', validateMail);                           
+    /************* controle de l'email **************/     
+    btnSubmit.addEventListener('click', validateMail);                           
         function validateMail(e) {                                                   
             if (!regexEmail.test(emailInput.value)) {
                 e.preventDefault();
                 errorMail.style.display = "block";
             } else {
                 errorMail.style.display = "none";
-                console.log(emailInput.value);
-                
+                console.log("Email : " + emailInput.value);    
             }
         }
 
-    /************* fonction controle message **************/
-    btnSubmit[1].addEventListener('click', validateMessage);                        
+    /************** controle du  message ***************/
+    btnSubmit.addEventListener('click', validateMessage);                        
         function validateMessage(e) {                                                
             if (!regexMessage.test(messageInput.value)) {                          
                 e.preventDefault();
-                errorMessage.style.display = "block";                
+                errorMessage.style.display = "block";               
             } else {
                 errorMessage.style.display = "none";
-                console.log(messageInput.value);
-               
-            }
-            
+                console.log("Message : " + messageInput.value);  
+            }    
         }
         
-    /************************** on réaffiche les éléments quand le formulaire est valide *************************************/    
-    btnSubmit[1].addEventListener('click', (e) => {                                  
-           
+    /** contrôle de tous les inputs au submit **/    
+    btnSubmit.addEventListener('click', (e) => {                                             
         e.preventDefault();
             if (regexFirstLast.test(firstNameInput.value && lastNameInput.value) && (regexEmail.test(emailInput.value)) && (regexMessage.test(messageInput.value))) {
-                redisplayElements()
-                resetForm()
-            }          
-        })
-    }
-     
-/************************************************ fonction fermeture du formulaire au ******************************************/
-//(dans photographer.html  ==>>  onkeydown="if (event.keyCode == 13) fermeture modale sur touche entrée )   
-function closeModal() {
-
-    const modal              = document.getElementById("contact_modal");
-    const headerLogo         = document.getElementsByTagName("header");                                                
-    const headerPhotographer = document.getElementsByClassName("photograph-header");                             
-    const filter             = document.getElementsByTagName("label");                                          
-    const mediaSection       = document.getElementById("media_section");
-    const firstNameInput     = document.getElementById("prenom");
-    const lastNameInput      = document.getElementById("nom");
-    const emailInput         = document.getElementById("email");
-    const messageInput       = document.getElementById("message");
-    const errorFirst         = document.getElementById("error-first");         // les balises d'erreur
-    const errorLast          = document.getElementById("error-last");
-    const errorMail          = document.getElementById("error-email");
-    const errorMessage       = document.getElementById("error-message"); 
-
-        /************* fonction pour vider toute les erreurs et tous les champs ****************/
-        function resetForm() {                                                      
-            firstNameInput.value                = " ";
-            lastNameInput.value                 = " ";
-            emailInput.value                    = " ";
-            messageInput.value                  = " ";
-            errorFirst.style.display            = "none";
-            errorLast.style.display             = "none";
-            errorMail.style.display             = "none";
-            errorMessage.style.display          = "none";
-        }
-
-    modal.style.display                 = "none";                              // on cache la modale
-    headerLogo[0].style.display         = "block";                             // on affiche header avec logo
-    headerLogo[0].style.display         = "flex";
-    headerPhotographer[0].style.display = "block";                             // on affiche le header du photographe 
-    headerPhotographer[0].style.display = "flex"; 
-    filter[0].style.display             = "block";                             // on affiche le filtre 
-    mediaSection.style.display          = "block";                             // on affiche la section media
-    mediaSection.style.display          = "flex";
-    resetForm();
-    
+                redisplayElements();
+                resetForm();
+            } else {
+                console.error('Formulaire non valide');
+            }        
+        });
 }
